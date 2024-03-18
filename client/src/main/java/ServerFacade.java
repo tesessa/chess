124eb42c8e3d0cc8cb8a6b31a507680c1ab61416
tesessa <tessaeliseandersen@yahoo.com>
2024-03-18 14:12:05 -0600

    phase 5 started

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,67 @@
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+import model.*;
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> resopnseClass) throws IOException {
+        try {
+            URL url = (new URL(serverUrl + path)).toURI().toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, resopnseClass);
+        } catch (Exception ex) {
+            throw new IOException();
+        }
+    }
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if(request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException {
+        var status = http.getResponseCode();
+        if(!isSuccessful(status)) {
+            throw new IOException();
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream resBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(resBody);
+                if(responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status/100 == 2;
+    }
+}
+
+
+
